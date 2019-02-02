ba49bbbbd68e894112a3d3876ccce651a7b5ef1d
nevooronni <nevooronni@gmail.com>
2019-02-03 02:38:52 +0300

    [finishes #163602280] added html css js for view user political votes feature

--- UI/js/script.js
+++ UI/js/script.js
@@ -2,16 +2,19 @@
 var modal = document.getElementById('myModal');
 var editModal = document.getElementById('myModal2');
 var deleteModal = document.getElementById("myModal3");
+var voteModal = document.getElementById("myModal4");
 
 // Get the button that opens the modal
 var btn = document.getElementById("createparty");
-var btn2 = document.getElementById('edit');
-var btn3 = document.getElementById('trash');
+var btn2 = document.getElementById("edit");
+var btn3 = document.getElementById("trash");
+var btn4 = document.getElementById("votebutton");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 var span2 = document.getElementsByClassName("close2")[0];
 var span3  = document.getElementsByClassName("close3")[0];
+var span4 = document.getElementsByClassName("close4")[0];
 
 // When the user clicks on the button, open the modal 
 btn.onclick = function() {
@@ -26,6 +29,10 @@ btn3.onclick = function() {
   deleteModal.style.display = "block";
 }
 
+btn4.onclick = function() {
+  voteModal.style.display = "block";
+}
+
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
@@ -37,6 +44,10 @@ span2.onclick = function() {
 
 span3.onclick = function() {
   deleteModal.style.display = "none";
+}
+
+span4.onclick = function() {
+  voteModal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
